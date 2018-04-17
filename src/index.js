bd7cea7f69e50d05521c58a88a7b0358db96282d
Reinaldo Schiehll <rn.schiehll@gmail.com>
2018-04-17 15:09:45 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+class MatchMedia extends React.Component {
+  state = {
+    query: this.props.query,
+    matches: window.matchMedia(this.props.query).matches
+  }
+
+  matchMediaListener = e => {
+    this.setState({
+      matches: e.matches
+    })
+  }
+
+  addListener(query) {
+    window.matchMedia(query).addListener(this.matchMediaListener)
+  }
+
+  removeListener(query) {
+    window.matchMedia(query).removeListener(this.matchMediaListener)
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.query !== prevState.query) {
+      return {
+        query: nextProps.query,
+        matches: window.matchMedia(nextProps.query).matches
+      }
+    }
+
+    return null
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { query } = this.state
+
+    if (prevState.query !== query) {
+      this.removeListener(query)
+      this.addListener(query)
+    }
+  }
+
+  componentDidMount() {
+    this.addListener(this.props.query)
+  }
+
+  componentWillUnmount() {
+    this.removeListener(this.props.query)
+  }
+
+  render() {
+    return this.props.children(this.state.matches)
+  }
+}
+
+export default MatchMedia
